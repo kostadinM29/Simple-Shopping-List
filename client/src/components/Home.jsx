15232d7085730b867bbda01997fdacc5f145d71b
Kostadin Minov <minovkostadin@gmail.com>
2024-01-29 22:17:46 +0200

    fluff

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,18 +2,19 @@ import { useAuth } from "../hooks/useAuth";
 
 const Home = () =>
 {
-    const { user, logout } = useAuth();
+    const { user } = useAuth();
 
     return (
-        <div>
-            {user ? (
-                <div>
-                    <p>Welcome, {user.username}!</p>
-                    <button onClick={logout}>Logout</button>
-                </div>
-            ) : (
-                <p>You are not logged in.</p>
-            )}
+        <div className="flex items-center justify-center h-screen">
+            <div className="text-center">
+                {user ? (
+                    <div>
+                        <h1 className="text-3xl font-bold mb-4">Welcome to your shopping list, {user.decodedToken.email}!</h1>
+                    </div>
+                ) : (
+                    <h1 className="text-3xl font-bold">Please log in to start creating your shopping lists!</h1>
+                )}
+            </div>
         </div>
     );
 };
